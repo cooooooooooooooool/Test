40549a92ff32d76460290ef735f6a0a3de5783e3
cooooooooooooooool <316178178@qq.com>
2018-09-06 15:35:46 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "main3.h"
+
+int main() {
+    int main2 = main3(5);
+    std::cout << "Hello, World!" << std::endl;
+    std::cout<<main2<<std::endl;
+    return 0; 
+}
